1e36b6b231853cd1a20b788baa7246fb5bf9a385
MOJUNSEO <mojunseo49@naver.com>
2019-12-14 06:13:24 +0900

    make api index

--- src/main/java/com/example/demo/events/EventController.java
+++ src/main/java/com/example/demo/events/EventController.java
@@ -1,5 +1,6 @@
 package com.example.demo.events;
 
+import com.example.demo.common.ErrorsResource;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.Link;
@@ -40,12 +41,12 @@ public class EventController {
     @PostMapping
     public ResponseEntity createEvent(@RequestBody @Valid EventDto eventDto, Errors errors) {
         if(errors.hasErrors()) {
-            return ResponseEntity.badRequest().body(errors);
+            return badRequest(errors);
         }
 
         eventValidator.validate(eventDto, errors);
         if(errors.hasErrors()) {
-            return ResponseEntity.badRequest().body(errors);
+            return badRequest(errors);
         }
 
 
@@ -70,5 +71,9 @@ public class EventController {
         eventResource.add(selfLinkBuilder.withRel("update-event"));
         eventResource.add(new Link("/docs/index.html#resource-events-create").withRel("profile"));
         return ResponseEntity.created(createdUri).body(eventResource);
+    }
+
+    private ResponseEntity badRequest(Errors errors) {
+        return ResponseEntity.badRequest().body(new ErrorsResource(errors));
     }
 }
--- src/test/java/com/example/demo/events/EventControllerTests.java
+++ src/test/java/com/example/demo/events/EventControllerTests.java
@@ -212,9 +212,10 @@ public class EventControllerTests {
                 .content(this.objectMapper.writeValueAsString(eventdto)))
                 .andDo(print())
                 .andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$[0].objectName").exists())
-                .andExpect(jsonPath("$[0].defaultMessage").exists())
-                .andExpect(jsonPath("$[0].code").exists())
+                .andExpect(jsonPath("content[0].objectName").exists())
+                .andExpect(jsonPath("content[0].defaultMessage").exists())
+                .andExpect(jsonPath("content[0].code").exists())
+                .andExpect(jsonPath("_links.index").exists());
         ;
     }
 
